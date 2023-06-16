6050b283aec8b157e72cea726a96ab97ee4131a3
= <vanduc10101010@gmail.com>
2023-06-17 01:58:35 +0700

    maivantien commit 1:58 17/6/2023

--- src/main/java/com/javaweb/QLktx/models/Student.java
+++ src/main/java/com/javaweb/QLktx/models/Student.java
@@ -1,18 +1,37 @@
 package com.javaweb.QLktx.models;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "sinh_vien")
 public class Student {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(name = "name")
-	private String name;
+	@Column(name = "ho_ten")
+	private String hoTen;
 
-	@Column(name = "roll_numbers")
-	private String rollNumber;
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "ma_so_sinh_vien")
+	private Integer maSV;
+	
+	@Column(name = "ngay_sinh")
+	private LocalDate ngaySinh;
+	
+	@Column(name = "gioi_tinh")
+	private String gioiTinh;
+	
+	@Column(name = "so_dien_thoai")
+	private Integer soDienThoai;
+
+	@ManyToOne
+    @JoinColumn(name = "id_phong")
+    private Room room;
 
 	public Long getId() {
 		return id;
@@ -22,19 +41,60 @@ public class Student {
 		this.id = id;
 	}
 
-	public String getName() {
-		return name;
+	public String getHoTen() {
+		return hoTen;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setHoTen(String hoTen) {
+		this.hoTen = hoTen;
 	}
 
-	public String getRollNumber() {
-		return rollNumber;
+	public String getEmail() {
+		return email;
 	}
 
-	public void setRollNumber(String rollNumber) {
-		this.rollNumber = rollNumber;
+	public void setEmail(String email) {
+		this.email = email;
 	}
+
+	public Integer getMaSV() {
+		return maSV;
+	}
+
+	public void setMaSV(Integer maSV) {
+		this.maSV = maSV;
+	}
+
+	public LocalDate getNgaySinh() {
+		return ngaySinh;
+	}
+
+	public void setNgaySinh(LocalDate ngaySinh) {
+		this.ngaySinh = ngaySinh;
+	}
+
+	public String getGioiTinh() {
+		return gioiTinh;
+	}
+
+	public void setGioiTinh(String gioiTinh) {
+		this.gioiTinh = gioiTinh;
+	}
+
+	public Integer getSoDienThoai() {
+		return soDienThoai;
+	}
+
+	public void setSoDienThoai(Integer soDienThoai) {
+		this.soDienThoai = soDienThoai;
+	}
+
+	public Room getRoom() {
+	    return room;
+	}
+
+	public void setRoom(Room room) {
+	    this.room = room;
+	}
+	
 }
